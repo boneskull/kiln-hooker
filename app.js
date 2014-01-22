cabf381a303b870fa5aa4f1813a88c6ddf9055a6
Christopher Hiller <chiller@decipherinc.com>
2014-01-21 19:39:36 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var express = require('express'),
+    expressWinston = require('express-winston'),
+    winston = require('winston'),
+    http = require('http'),
+    path = require('path'),
+    app,
+    log,
+    transportOptions = {
+        colorize: true,
+        timestamp: true
+    };
+
+app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3117);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console(transportOptions)
+    ]
+}));
+
+// development only
+if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+log = new winston.Logger({transports: [new winston.transports.Console(transportOptions)]});
+
+
+app.get('/', function (req, res) {
+    log.info(req);
+    res.status(200);
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+    log.info('Express server listening on port ' + app.get('port'));
+
+
+});
